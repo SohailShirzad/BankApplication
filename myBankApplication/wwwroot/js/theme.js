2667f8d37c8086353c98e8564f6e76975d9e93f5
sohail <as19adj@herts.ac.uk>
2023-02-05 05:48:03 +0000

    added layout theme changer and home page image

---
+++ myBankApplication/wwwroot/js/theme.js
@@ -0,0 +1,15 @@
+﻿function white() {
+	var element = document.getElementById("changeTheme");
+	element.classList.add("white");
+	element.classList.remove("black","purple");
+}
+function black() {
+	var element = document.getElementById("changeTheme");
+	element.classList.add("black");
+	element.classList.remove("white","purple");
+}
+function purple() {
+	var element = document.getElementById("changeTheme");
+	element.classList.add("purple");
+	element.classList.remove("white","black");
+}
